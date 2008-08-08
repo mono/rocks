83c157bd8d5752bd9d5355ae9f8ad5e10d1bcd89
Jonathan Pryor <jpryor@novell.com>
2008-08-08 00:39:43 +0000

    * Mono.Rocks.dll.sources: Add Mono.Rocks/Stream.cs to the build. 	* Mono.Rocks.Tests.dll.sources: Add Tests/Mono.Rocks.Tests/StreamTest.cs 	  to the build. 	* Mono.Rocks/Stream.cs: Added; .AsEnumerable() extension method for 	  System.IO.Stream. 	* Tests/Mono.Rocks.Tests/StreamTests.cs: Added; unit tests for 	  Mono.Rocks.StreamRocks.AsEnumerable.
    svn path=/branches/rocks-playground/; revision=109924

---
+++ Tests/Mono.Rocks.Tests/StreamTest.cs
@@ -0,0 +1,61 @@
+//
+// StreamTest.cs
+//
+// Author:
+//   Jonathan Pryor  <jpryor@novell.com>
+//
+// Copyright (c) 2008 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Mono.Rocks;
+
+namespace Mono.Rocks.Tests {
+
+	[TestFixture]
+	public class StreamTest : BaseRocksFixture {
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void AsEnumerable_SelfNull ()
+		{
+			Stream s = null;
+			s.AsEnumerable ();
+		}
+
+		[Test]
+		public void AsEnumerable()
+		{
+			MemoryStream ms = new MemoryStream (
+					Encoding.UTF8.GetBytes ("Hello, World!"));
+			Assert.AreEqual ("Hello, World!",
+					ms.AsEnumerable ().Select (b => (char) b).Implode ());
+		}
+	}
+}
